620c41932b082e2944c0b4b7b580903101b02da4
vitalie.jicol <vitaliejicol89@gmail.com>
2025-06-27 13:30:44 -0500

    Added destroy

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-830c94e3"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
+#   tags = {
+#     Name = "ExampleAppServerInstance"
+#   }
+# }
